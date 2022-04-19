bc991ec0972129545444b58d7dcfbac01b1e20b8
Alejandroav <alv12429@uvb.edu.gt>
2022-04-19 16:53:02 -0600

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Proyecto_DiscordSite
+Proyecto_DiscordSite:
+Discord site replica attempt implementing:
+*HTML
+*JavaScript
+*CSS
+*React
+*Babel
+*Webpack
+*Eslint (Airbnb, Prettier) 
--- src/components/discordSite.js
+++ src/components/discordSite.js
@@ -9,13 +9,17 @@ class DiscordSite extends React.Component {
     return (
       <div className="mainpanel">
         <div className="panel1">
-            <img className="panel1img" src="https://techcrunch.com/wp-content/uploads/2021/05/Discord_IAP_KeyVisuals_Header_02.jpeg"></img>
-          <div className="panel2"><p>This is some text in a div element.</p>
-            <div className="panel3"><p>This is some text in a div element.</p>
-              <div className="panel4"><p>This is some text in a div element.</p>
-                <div className="panel5"><p>This is some text in a div element.</p>
-                  <div className="panel6">
+          <img className="panel1img" src="https://techcrunch.com/wp-content/uploads/2021/05/Discord_IAP_KeyVisuals_Header_02.jpeg" />
+          <div className="panel2">
+            <p>This is some text in a div element.</p>
+            <div className="panel3">
+              <p>This is some text in a div element.</p>
+              <div className="panel4">
+                <p>This is some text in a div element.</p>
+                <div className="panel5">
                   <p>This is some text in a div element.</p>
+                  <div className="panel6">
+                    <p>This is some text in a div element.</p>
                   </div>
                 </div>
               </div>
