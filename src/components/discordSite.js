4edc4bca15a9398246e028558552164299c7fbd1
Alejandroav <alv12429@uvb.edu.gt>
2022-04-19 11:22:13 -0600

    created js for site rendering

---
+++ src/components/discordSite.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './discordSite.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class DiscordSite extends React.Component {
+
+}
+
+export default DiscordSite;
